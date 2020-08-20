0368d785d8ffb1c25847fb0bfa92a3b83ce0120a
licong <licong@qianxin.com>
2020-08-20 10:31:54 +0800

    fix log

--- main.go
+++ main.go
@@ -4,17 +4,24 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 var dir string
+var port string
 
-func main() {
-	port := flag.String("p", "8100", "port to serve on")
+func init() {
+	flag.StringVar(&port, "p", "8100", "port to serve on")
 	flag.StringVar(&dir, "path", "./", "the directory of static file to host")
 	flag.Parse()
+	if dir == "./" {
+		dir, _ = os.Getwd()
+	}
+}
+func main() {
 
 	http.Handle("/", FileServer(Dir(dir)))
 
-	log.Printf("Serving %s on HTTP port: %s\n", dir, *port)
-	log.Fatal(http.ListenAndServe(":"+*port, nil))
+	log.Printf("Serving %s on HTTP port: %s\n", dir, port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
