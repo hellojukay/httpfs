6758a64c94a5073fd1461f796490a38ae2162382
hellojukay <licong@qianxin.com>
2021-01-04 18:40:14 +0800

    fix ci

--- build.pl
+++ build.pl
@@ -4,5 +4,5 @@ use strict;
 use warnings;
 
 my $arch="darwin/amd64 linux/386 linux/amd64 windows/amd64 windows/386";
-system("gox	-osarch=\"$arch\"  -output=\"dist/bin/{{.Dir}}_{{.OS}}_{{.Arch}}\"");
+system("gox	-osarch=\"$arch\"  -output=\"dist/{{.Dir}}_{{.OS}}_{{.Arch}}\"");
 
