459a469d216fe1d0a2bd12af4facb29403a14086
hellojukay <licong@qianxin.com>
2021-06-11 09:51:58 +0800

    修改 build.pl 兼容没有 gox 命令的情况

--- build.pl
+++ build.pl
@@ -3,6 +3,18 @@
 use strict;
 use warnings;
 
-my $arch="darwin/amd64 linux/386 linux/amd64 windows/amd64 windows/386";
-system("gox	-osarch=\"$arch\"  -output=\"dist/{{.Dir}}_{{.OS}}_{{.Arch}}\"");
+sub command_exsits {
+    my $cmd = shift;
+    my $exit = system("which $cmd > /dev/null 2>&1");
+    return $exit == 0;
+}
+
+if(command_exsits('gox')) {
+    print("[INFO] gox command found , now run gox build.\n");
+    my $arch="darwin/arm64 darwin/amd64 linux/386 linux/amd64 windows/amd64 windows/386";
+    system("gox	-osarch=\"$arch\"  -output=\"dist/{{.Dir}}_{{.OS}}_{{.Arch}}\"");
+} else {
+    print("[WARN] gox command not founnd, runing go build.\n");
+    system("go build");
+}
 
