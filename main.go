5e19e2cd86b34725fb7b3c57afa595ad53654d64
hellojukay <hellojukay@163.com>
2022-01-23 08:44:49 +0000

    支持显示版本 --version

--- main.go
+++ main.go
@@ -6,15 +6,26 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"runtime/debug"
 )
 
 var dir string
 var port string
+var version bool
 
 func init() {
 	flag.StringVar(&port, "p", "8100", "port to serve on")
 	flag.StringVar(&dir, "path", "./", "the directory of static file to host")
+	flag.BoolVar(&version, "version", false, "print program version")
 	flag.Parse()
+
+	if version {
+		info, ok := debug.ReadBuildInfo()
+		if ok {
+			println(info.Main.Version, info.Main.Sum)
+		}
+		os.Exit(0)
+	}
 	if dir == "./" {
 		dir, _ = os.Getwd()
 	}
