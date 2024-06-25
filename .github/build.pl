11cb898f937e2e7133d5f5e7259ad7f494d8ec30
cong.li <cong.li@jiduauto.com>
2024-06-25 17:44:16 +0800

    update

--- .github/build.pl
+++ .github/build.pl
@@ -11,7 +11,7 @@ sub command_exsits {
 
 $ENV{CGO_ENABLED} = 0;
 if(command_exsits('gox')) {
-    system("go install github.com/mitchellh/gox@latest");
+    system("go install github.com/mitchellh/gox@v1.0.1");
     print("[INFO] gox command found , now run gox build.\n");
     my $arch="darwin/arm64 darwin/amd64 linux/386 linux/amd64 windows/amd64 windows/386";
     system("gox	-osarch=\"$arch\"  -output=\"dist/{{.Dir}}_{{.OS}}_{{.Arch}}\"");
